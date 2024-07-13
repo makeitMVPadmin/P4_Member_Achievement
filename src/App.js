68c4a0b64dc7bb2e566c24c2d8b8844802211632
Yvv1214 <94neongreen@gmail.com>
2024-07-13 18:33:59 -0400

    syntax error fix

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage/HomePage.jsx";
 import Header from "./components/Header/Header.jsx";
 
-function App() {
+const App = () =>  {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter>)
+}
 
 
 export default App;

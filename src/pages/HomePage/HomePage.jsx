5aa33c2901e43a1498977c3efb722b67adcfc7fc
hilarykhc <hilary.kh.chen@gmail.com>
2024-08-02 03:49:30 -0400

    update homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,18 @@
 import "./HomePage.scss";
 import Onboarding from "../../components/Onboarding/Onboarding";
 import { useState, useEffect } from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import blueWaveImg from "../../assets/images/blue-wave.png";
 
 const HomePage = () => {
   const [isModalOpen, setIsModalOpen] = useState(true); //
+  const [resources, setResources] = useState([]);
+  const [selectedResource, setSelectedResource] = useState(null);
+  const [category, setCategory] = useState("All");
+  const [type, setType] = useState([]);
+  const [activeResourceId, setActiveResourceId] = useState(null);
+  const [sortField, setSortField] = useState(null);
+  const [sortAscending, setSortAscending] = useState(true);
 
   useEffect(() => {
     const hasSeenModal = localStorage.getItem("hasSeenOnboardingModal");
@@ -18,8 +27,44 @@ const HomePage = () => {
     localStorage.setItem("hasSeenOnboardingModal", "true");
   };
 
+  const handleFormSubmit = (newResource) => {
+    const updatedResources = [...resources, newResource];
+    setResources(updatedResources);
+    console.log("Updated Resources:", updatedResources);
+    localStorage.setItem("resources", JSON.stringify(updatedResources));
+
+    if (!selectedResource) {
+      setSelectedResource(newResource);
+      setActiveResourceId(newResource.id);
+    }
+  };
+
+  const sortSkill = () => {
+    setSortField("skill");
+    setSortAscending(!sortAscending);
+  };
+
+  const sortDuration = () => {
+    setSortField("duration");
+    setSortAscending(!sortAscending);
+  };
+
   return (
     <div className="homepage">
+      <img
+        className="homepage__img"
+        src={blueWaveImg}
+        alt="blue wave background"
+      />
+      <div className="resource__navbar-container">
+        <NavBar
+          onTypeChange={setType}
+          onCategoryChange={setCategory}
+          onFormSubmit={handleFormSubmit}
+          sortBySkill={sortSkill}
+          sortByDuration={sortDuration}
+        />
+      </div>
       <Onboarding
         // className="homepage__onboarding"
         isOpen={isModalOpen}
@@ -29,7 +74,9 @@ const HomePage = () => {
           width: "40rem",
         }}
       />
-      <h1>welcome to the community library</h1>
+      <h1 className="homepage__title">
+        Welcome to the Communiti Resource Library
+      </h1>
     </div>
   );
 };

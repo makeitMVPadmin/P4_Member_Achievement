7e4baed5d62b0f1877598fef23d14b256cb6ccd7
hilarykhc <hilary.kh.chen@gmail.com>
2024-08-02 22:18:58 -0400

    update upvote and comment icons on resource cards

--- src/components/ResourceCard/ResourceCard.jsx
+++ src/components/ResourceCard/ResourceCard.jsx
@@ -5,9 +5,11 @@ import arrowForwardIcon from "../../assets/icons/blue-arrow-forward-svgrepo-com.
 import timerIcon from "../../assets/icons/timer.png";
 import Upvoting from "../Upvoting/Upvoting";
 import "./ResourceCard.scss";
+import upvoteImg from "../../assets/images/upvote.png";
+import commentsImg from "../../assets/images/comments.png";
 
 export default function ResourceCard(props) {
-  const { resource, selectResource, isActive } = props;
+  const { resource, selectResource, isActive, comments } = props;
 
   const handleClickCard = () => {
     selectResource(resource.id);
@@ -42,7 +44,26 @@ export default function ResourceCard(props) {
       </div>
       <div className="resource__heading-bottom">
         <h1 className="resource__title">{resource.title}</h1>
-        <Upvoting />
+        <div className="resource__icons-container">
+          <div className="resource__icons">
+            <img
+              className="resource__icons-img"
+              src={upvoteImg}
+              alt="upvote icon"
+            />
+            <p className="resource__upvotes-total">0</p>
+          </div>
+          <div className="resource__icons">
+            <img
+              className="resource__icons-img"
+              src={commentsImg}
+              alt="comments icon"
+            />
+            <p className="resource__comments-total">{comments.length}</p>
+          </div>
+        </div>
+
+        {/* <Upvoting /> */}
       </div>
       <p className="resource__level">{resource.level}</p>
       <div className="resource__about">

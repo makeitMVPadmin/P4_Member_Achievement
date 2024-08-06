fc76114677a6eccc7b5ef2f1271eda9c5e299570
hilarykhc <hilary.kh.chen@gmail.com>
2024-08-05 23:49:53 -0400

    updated onboarding messages and updated success message after commenting

--- src/components/CommentModal/CommentModal.jsx
+++ src/components/CommentModal/CommentModal.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import modalClose from "../../assets/icons/modalClose.svg";
-import './CommentModal.scss'
+import "./CommentModal.scss";
 
-
-
-
-export const CommentModal = ({closeModal}) => {
-
-
-
-    return(
-        <div className="modal">
-            <img className="modalX" src={modalClose} alt="closeModal" onClick={closeModal} aria-label="close modal"/>
-            <h2 className="modalHeader">Success!</h2>
-            <p className="modalText">Thank you for contributing to the community-driven resource library! Your resource has been uploaded.</p>
-        </div>
-    )
-}
+export const CommentModal = ({ closeModal }) => {
+  return (
+    <div className="modal">
+      <img
+        className="modalX"
+        src={modalClose}
+        alt="closeModal"
+        onClick={closeModal}
+        aria-label="close modal"
+      />
+      <h2 className="modalHeader">Success!</h2>
+      <p className="modalText">Thank you for! Your comment has been posted.</p>
+    </div>
+  );
+};

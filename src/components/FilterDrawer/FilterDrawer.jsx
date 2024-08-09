50e4343f2141c96f525c5fc7fe116706b5327c0a
SammyLug <samlugtu777@gmail.com>
2024-08-08 20:09:27 -0400

    moved filterDrawer to the right

--- src/components/FilterDrawer/FilterDrawer.jsx
+++ src/components/FilterDrawer/FilterDrawer.jsx
@@ -73,7 +73,7 @@ export default function FilterDrawer({ onFilterChange }) {
       <div>
         <Drawer
           isOpen={isOpen}
-          placement="left"
+          placement="right"
           onClose={handleCancel}
           size="sm"
         >

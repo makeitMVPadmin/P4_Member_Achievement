346d3847fdbf34e8749d569bc523bbe2afa98876
zahfir <zafy2001@gmail.com>
2024-10-02 14:32:13 -0400

    firebase setup

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -17,10 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getFirestore(app);
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
 
-// Use Firestore emulator in development
-// if (window.location.hostname === "localhost") {
-//   connectFirestoreEmulator(database, "127.0.0.1", 8080);
-// }
-
-export { database, storage };
+export { database, storage, analytics };
